bf140beac601482b1f372dfae7d01845cb195fa9
Miguel Angel Huerta Fernandez <mikehf7@gmail.com>
2012-05-29 09:52:00 -0500

    ipasa primer aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipasa::Application.config.secret_token = '0daafcb7d1c3d493a14a1e80ad066ccf51ae9347e2532f5d8a312040c731ae557f5b214750df09101264a2e1d187e73cebac6dcbb22e02a21010ba9a91af6f4b'
